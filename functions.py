c85d900325637940725f422fa06dcc179bf8e263
Felipe Heitmann <felipeph@gmail.com>
2023-09-17 21:11:23 -0300

    Novo app mais simples

---
+++ functions.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pandas as pd
+
+# Pytz para ajuste de timezone
+import pytz
+
+# Datetime para pegar a hora e a data atual
+from datetime import datetime
+
+from pathlib import Path
+
+
+def load_or_create_df(csv_file, columns_names_list):
+    """
+    Load a dataframe from a CSV if it exists or create an empty dataframe from a list of name of columns
+    
+    Args:
+        csv_file (str): The file path of the CSV to create a dataframe from
+        columns_names_list (list): List of names for the header of the new dataframe
+        
+    Returns:
+        dataframe : Empty or full, depending on the CSV file given
+        
+    """
+    csv_file_path = Path(csv_file)
+    try:
+        dataframe = pd.read_csv(csv_file_path)
+    except FileNotFoundError:
+        dataframe = pd.DataFrame(columns=columns_names_list)
+
+    return dataframe
+
+def new_row_to_csv(csv_file, dataframe, df_columns_names, new_row_values):
+    """
+    Add a new row to a dataframe and save it into the CSV file
+    
+    Args:
+        csv_file (str): The file path of the CSV file to save into.
+        dataframe (dataframe): Original Pandas dataframe to include de new row
+        df_columns_names (list): List of names for the header of the new dataframe
+        new_row_values (list): List of values with for the columns
+    
+    Returns:
+        status
+    
+    """
+
+    try:
+        new_row_dict = dict(zip(df_columns_names, new_row_values))
+        new_row_df = pd.DataFrame(new_row_dict, index=[0])
+        dataframe = pd.concat([new_row_df, dataframe], ignore_index=True)
+        dataframe.to_csv(csv_file, index=False)
+    except Exception as e:
+        st.error(f"Ocorreu um erro: {e}.")
+    return None
+
+def time_now():
+    """
+    Return the time right now in Brazil official timezone
+    """
+    return datetime.now(pytz.timezone('America/Sao_Paulo'))
+    
+def last_record_time(dataframe):
+    # Verifica se dataframe não está vazio
+    if not dataframe.empty:
+        
+        # Obtenha a data e hora do fim da atividade mais recente
+        last_record_time = dataframe['date_time_end'].iloc[0]
+    else:
+        # Se não houver registros anteriores, use o horário atual como início
+        last_record_time = time_now()
+    return last_record_time
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,36 @@
 import streamlit as st
 
-st.title("Diário de Atividades")
+from functions import load_or_create_df, new_row_to_csv, time_now, last_record_time
+
+records_file = "data/records.csv"
+records_columns_names = ["description",
+                          "date_time_end",
+                          "date_time_start",
+                          ]
+st.session_state["records_df"] = load_or_create_df(records_file, records_columns_names)
+records_df = st.session_state["records_df"]
+
+
+with st.form(key="Novo Registro", clear_on_submit=True):
+    new_record_description = st.text_input("Descrição")
+    now = time_now()
+    last_record_end_time = last_record_time(records_df)
+    new_record_values = [new_record_description,
+                         now,
+                         last_record_end_time,
+                         ]
+    new_record_submitted = st.form_submit_button(label="Salvar", use_container_width=True)
+
+if new_record_submitted:
+    new_row_to_csv(records_file, records_df, records_columns_names, new_record_values)
+    st.success("Novo registro realizado com sucesso.")
+    st.experimental_rerun()
+
+st.table(records_df)
+
+    
+# ---------------- Debug---------------------
+st.sidebar.write(st.session_state)
+st.sidebar.write(records_df)
+st.sidebar.write(records_df.dtypes)
+# -------------------------------------------
